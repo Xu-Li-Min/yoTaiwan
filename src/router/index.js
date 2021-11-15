d7fb8e771cfb96ef82312eea564c4147587f37e9
LiMin <a0952579528@gmail.com>
2021-11-15 11:05:46 +0800

    npn install

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("../views/Home.vue"),
+    },
+
+];
+
+export default createRouter({
+    history: createWebHistory(),
+    routes,
+});
